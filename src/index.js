9d2ab627d877c15415497e79c6f1d9c05d875892
Branko Cvetanović <cbranko01@gmail.com>
2023-08-31 00:50:44 +0200

    Make the basic sceleton for the project, add html an css for home page

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import './style.css';
+import Icon from './icon.png';
+
+ const iconPlace=document.querySelector(".icon")
+
+ const myIcon = new Image();
+ myIcon.src = Icon;
+
+ iconPlace.appendChild(myIcon);
+
+ import Icon1 from './IMG_5142-min.png';
+
+ const picHolder=document.querySelector(".pic")
+
+ const myIcon1 = new Image();
+ myIcon1.src = Icon1;
+
+ picHolder.appendChild(myIcon1);
